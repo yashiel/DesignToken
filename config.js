bb36ba4b57bf276c25764f63aaaa9f0384abfad1
Yashi EL <hi@yashiel.com>
2023-01-31 19:22:40 +0530

    configjs modified

--- config.js
+++ config.js
@@ -1,57 +1,53 @@
+const StyleDictionary = require('style-dictionary');
 
-const { formatHelpers } = StyleDictionary;
-
-const traverseObj = (obj) => {
-	let output = {};
-	if (obj.hasOwnProperty('value')) {
-		return {
-			value: obj.value,
-			type: obj.type,
-			description: obj.description,
-		};
-	} else if (typeof obj === 'object') {
-		// check if the current node holds an object
-		// if it does, recursively run through the object as well
-		Object.keys(obj).forEach((key) => {
-			output[key] = traverseObj(obj[key]);
-		});
-	}
-	return output;
-};
-
+// StyleDictionary.registerTransform({
+// 	type: `value`,
+// 	name: `typography/font`,
+// 	transitive: true,
+// 	matcher: (token) => {
+// 		return token.type === 'typography';
+// 	},
+// 	transformer: (token) => {
+// 		return `${token.original.value.fontWeight} ${token.original.value.fontSize}/${token.original.value.lineHeight} ${token.original.value.fontFamily}`;
+// 	},
+// });
 StyleDictionary.registerTransform({
 	type: `value`,
-	name: `typography/font`,
+	name: `scale/unit`,
 	transitive: true,
 	matcher: (token) => {
-		return token.type === 'typography';
+		return token.type === 'fontSizes' || token.type === 'spacing';
 	},
 	transformer: (token) => {
-		return `${token.original.value.fontWeight} ${token.original.value.fontSize}/${token.original.value.lineHeight} ${token.original.value.fontFamily}`;
+		const baseFont = 14; // !important base font size
+		const floatVal = parseFloat(token.value);
+
+		if (isNaN(floatVal)) {
+			throwSizeError(token.name, token.value, 'rem');
+		}
+
+		if (floatVal === 0) {
+			return '0';
+		}
+
+		return `${floatVal / baseFont}rem`;
 	},
 });
 
 module.exports = {
 	source: ['tokens/**/*.@(js|json)'],
-	format: {
-		figmatokens: ({ dictionary }) => {
-			return JSON.stringify(
-				{ Tokens: traverseObj(dictionary.tokens) },
-				null,
-				2
-			);
-		},
-	},
+
 	platforms: {
 		scss: {
+			basePxFontSize: 14,
 			transforms: [
 				'attribute/cti',
 				'name/cti/kebab',
 				'time/seconds',
 				'content/icon',
-				'size/rem',
+				'size/pxToRem',
 				'color/css',
-				'typography/font',
+				'scale/unit',
 			],
 			files: [
 				{
@@ -59,35 +55,22 @@ module.exports = {
 					format: 'scss/variables',
 				},
 			],
-			output: true,
 		},
 		css: {
-			//   transformGroup: 'css',
+			basePxFontSize: 14,
 			transforms: [
 				'attribute/cti',
 				'name/cti/kebab',
 				'time/seconds',
 				'content/icon',
-				'size/rem',
+				'size/pxToRem',
 				'color/css',
-				'typography/font',
+				'scale/unit',
 			],
 			files: [
 				{
 					destination: 'build/css/variables.css',
 					format: 'css/variables',
-				},
-			],
-			output: true,
-		},
-		figma: {
-			buildPath: 'build/',
-			basePxFontSize: 14,
-			transforms: ['name/cti/kebab', 'size/rem'],
-			files: [
-				{
-					destination: 'figma/figma.tokens.json',
-					format: 'figmatokens',
 				},
 			],
 		},
