246113a8967a7494dad30260177c23d38f6c43a3
yashiel <yashiel@users.noreply.github.com>
2023-02-03 14:50:48 +0000

    Update Design Tokens

--- designTokens/ios-swift/DesignSystem+Enum.swift
+++ designTokens/ios-swift/DesignSystem+Enum.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 03 Feb 2023 14:34:03 GMT
+// Generated on Fri, 03 Feb 2023 14:50:47 GMT
 
 
 import UIKit
--- designTokens/ios/DesignSystemSize.h
+++ designTokens/ios/DesignSystemSize.h
@@ -3,7 +3,7 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 03 Feb 2023 14:34:03 GMT
+// Generated on Fri, 03 Feb 2023 14:50:47 GMT
 
 
 #import <Foundation/Foundation.h>
